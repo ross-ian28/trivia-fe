727f8d917870cc82c89e6314e2dc83abe0d1bd85
Ian Ross <ianrossdrums@gmail.com>
2023-04-25 13:00:10 -0600

    Add result css

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from "react-router-dom";
-
+import "./Result.css"
 export default function Result() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const [questions, setQuestions] = useState(JSON.parse(searchParams.get("data")));
+  const questions = JSON.parse(searchParams.get("data"));
 
   return (
-    <div>
+    <div className="result-container">
       <h2>"You finished the quiz!"</h2>
       <h3>You got a { questions.score }/10</h3>
+      <a href="/"><button className="back-to-home-button">Play Again?</button></a>
     </div>
   );
 }
