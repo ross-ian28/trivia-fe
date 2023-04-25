1df5cd046c508c8d8610d983e63802d7b4f0d842
Ian Ross <ianrossdrums@gmail.com>
2023-04-24 21:44:37 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Intro from "./Intro.js";
+import Question1 from "./Questions/Question1.js";
+import Question2 from "./Questions/Question2.js";
+import Question3 from "./Questions/Question3.js";
+import Question4 from "./Questions/Question4.js";
+import Question5 from "./Questions/Question5.js";
+import Question6 from "./Questions/Question6.js";
+import Question7 from "./Questions/Question7.js";
+import Question8 from "./Questions/Question8.js";
+import Question9 from "./Questions/Question9.js";
+import Question10 from "./Questions/Question10.js";
+import Result from "./Results.js";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Intro />}></Route>
+          <Route path="/question1" element={<Question1 />}></Route>
+          <Route path="/question2" element={<Question2 />}></Route>
+          <Route path="/question3" element={<Question3 />}></Route>
+          <Route path="/question4" element={<Question4 />}></Route>
+          <Route path="/question5" element={<Question5 />}></Route>
+          <Route path="/question6" element={<Question6 />}></Route>
+          <Route path="/question7" element={<Question7 />}></Route>
+          <Route path="/question8" element={<Question8 />}></Route>
+          <Route path="/question9" element={<Question9 />}></Route>
+          <Route path="/question10" element={<Question10 />}></Route>
+          <Route path="/results" element={<Result />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
